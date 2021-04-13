087493c19cc4f615aa503847038005f0e246d83f
eunjae-lee <karis612@gmail.com>
2021-04-13 15:04:36 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Search />
+</template>
+
+<script setup>
+import Search from "./components/Search.vue";
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 2rem;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <ais-instant-search :search-client="searchClient" index-name="demo_ecommerce">
+    <ais-search-box />
+    <ais-hits>
+      <div slot="item" slot-scope="{ item }">
+        <h2>{{ item.name }}</h2>
+      </div>
+    </ais-hits>
+  </ais-instant-search>
+</template>
+
+<script setup>
+import algoliasearch from "algoliasearch/lite";
+import "instantsearch.css/themes/satellite-min.css";
+import { AisInstantSearch, AisSearchBox, AisHits } from "vue-instantsearch";
+
+const searchClient = algoliasearch(
+  "B1G2GM9NG0",
+  "aadef574be1f9252bb48d4ea09b5cfe5"
+);
+</script>
